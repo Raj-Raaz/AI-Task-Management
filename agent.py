5d378dd11e069916df0c8e016dfcd2f6e49fd9f0
Raj-Raaz <22f1000634@ds.study.iitm.ac.in>
2026-09-18 22:33:13 +0530

    confuguration and agent

--- agent.py
+++ agent.py
@@ -4,12 +4,13 @@ from langchain_groq import ChatGroq
 from tools import *
 from dotenv import load_dotenv
 from database import init_db
+import os
 
 load_dotenv()
 init_db()
 
-
-llm = ChatGroq(model="openai/gpt-oss-20b")
+MODEL= os.getenv("MODEL")
+llm = ChatGroq(model=MODEL)
 
 all_tools = [create_todo, list_todos, update_todos, delete_todo]
 
